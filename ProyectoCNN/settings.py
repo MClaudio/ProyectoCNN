5c67075286db41125085cf8f4bfa7147ad0c9dca
Claudio Maldonado <claudio.mldo@outlook.com>
2020-06-05 00:23:21 -0500

    archivo de librerias modificado

--- ProyectoCNN/settings.py
+++ ProyectoCNN/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'mm-24kvl%+rz5_&dmyoq=jtroswlnjn&la@81(cfdrvb14z&x4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
